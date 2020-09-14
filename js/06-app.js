79873de4330f8847727f478eb6907a373aa05cd0
Luis Angel <santosangel748@gmail.com>
2020-09-14 15:18:29 -0500

    Agregando SpeechApi

---
+++ js/06-app.js
@@ -0,0 +1,34 @@
+const salida=document.querySelector('#salida');
+const microfono=document.querySelector('#microfono');
+
+microfono.addEventListener('click', ejecutarSpeedAPI);
+
+function ejecutarSpeedAPI(){
+    const SpeechRecognition=webkitSpeechRecognition;
+    const recognition=new SpeechRecognition();
+
+    recognition.start();
+
+    recognition.onstart=function(){
+        salida.classList.add('mostrar');
+        salida.textContent='Escuchando...';
+    };
+    recognition.onspeechend=function(){
+        salida.textContent='Se dejo de grabar...';
+        recognition.stop();
+    }
+    recognition.onresult=function(e){
+        console.log(e.results[0][0]);
+
+        const {confidence, transcript} = e.results[0][0];
+
+        const speech=document.createElement('p');
+        speech.innerHTML='Grabado: ${transcript}';
+
+        const seguridad=document.createElement('p');
+        seguridad.innerHTML='Seguridad: ${parseint(confidence*100)}';
+
+        salida.appendChild(speech);
+        salida.appendChild(seguridad);
+    }
+}
