39dc7eefdb0d9ae4428339eb365b465e741150ad
Luis Angel <santosangel748@gmail.com>
2020-09-14 14:27:45 -0500

    Funcionalidad del boton Notificarme y Ver Notificacion

---
+++ js/01-app.js
@@ -0,0 +1,23 @@
+const notificarBtn=document.querySelector('#notificar');
+
+notificarBtn.addEventListener('click', () =>{
+    Notification
+        .requestPermission()
+        .then(resultado=>{
+            console.log('El resultado es', resultado );
+        });
+});
+
+const verNotificacion = document.querySelector('#verNotificacion');
+verNotificacion.addEventListener('click', () => {
+    if (Notification.permission === 'granted') {
+        new Notification('Esta es la notificacion', {
+            icon: 'img/chems.jpg',
+            body: 'Codigo con Angel'
+        });
+
+        notificacion.onclick = function(){
+            window.open('https://github.com/Luis-Angel-Santos')
+        }
+    }
+});
